946a4dcd61603fced631aa76ce82eb1dac52c6b9
LechMael <63647566+LechMael@users.noreply.github.com>
2020-12-23 21:55:27 +0200

    Am adaugat comentarii de tipul single line

--- src/ro/java/ctrln/Main.java
+++ src/ro/java/ctrln/Main.java
@@ -1,8 +1,9 @@
 package ro.java.ctrln;
 
 public class Main {
-    //JAR - Java Archive
+    //Mai jos avem metod main care porneste o aplicatie Java
     public static void main(String[] args) {
+        //Prin System.out.println vrem sa printam in consola IntelliJ string-ul de mai jos
 	    System.out.println("Invat limbajul Java si despre masina virtuala Java!");
     }
 }
