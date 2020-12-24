157af4f8a4ea728a5dbee67c350e59f8e8d36cd2
LechMael <63647566+LechMael@users.noreply.github.com>
2020-12-24 12:13:56 +0200

    Am adaugat JavaDoc

--- src/ro/java/ctrln/Main.java
+++ src/ro/java/ctrln/Main.java
@@ -7,6 +7,13 @@ public class Main {
     de comentariu pe mai
     multle linii
      */
+
+    /**
+     * Metoda main este metoda apelata de masina virtuala Java si care porneste o aplicatie Java
+     * Putem sa transmitem argumente aditionale prin parametrul <strong>args</strong>
+     * @param args Acesta este parametrul cu care transmitem argumente programului nostru
+     * @author Toma Tudor
+     */
     public static void main(/* args este parametrul medtodei main*/String[] args) {
         /*Prin System.out.println vrem sa
         printam in consola IntelliJ string-ul de mai jos
